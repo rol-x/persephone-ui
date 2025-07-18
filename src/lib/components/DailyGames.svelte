01202863824b1159cfa107459cab1e482cf4936f
Karol Latos <pl.charlessummers@gmail.com>
2025-07-18 14:52:43 +0200

    Add more games

--- src/lib/components/DailyGames.svelte
+++ src/lib/components/DailyGames.svelte
@@ -1,19 +1,19 @@
 <script>
   const games = [
     {
-      name: "Wordle",
-      url: "https://www.nytimes.com/games/wordle",
-      description: "Zgadnij 5-literowe słowo dnia"
-    },
-    {
       name: "Connections",
       url: "https://www.nytimes.com/games/connections",
       description: "Grupuj powiązane słowa"
     },
     {
+      name: "Wordle",
+      url: "https://www.nytimes.com/games/wordle",
+      description: "Zgadnij 5-literowe słowo dnia"
+    },
+    {
       name: "Bandle",
       url: "https://bandle.app",
-      description: "Wordle o bohaterach LoL"
+      description: "Odgadnij piosenkę po instrumentach"
     },
     {
       name: "Globle",
@@ -24,12 +24,142 @@
       name: "Framed",
       url: "https://framed.wtf",
       description: "Zgadnij film na podstawie kadru"
+    },
+    {
+      name: "Movie to Movie",
+      url: "https://movietomovie.com/",
+      description: "Znajdź połączenie z jednego filmu do drugiego"
+    },
+    {
+      name: "Raddle",
+      url: "https://raddle.quest/",
+      description: "Wstaw słowa w podpowiedzi"
+    },
+    {
+      name: "Wiki Game",
+      url: "https://www.thewikigame.com/",
+      description: "Połącz ze sobą artykuły z Wikipedii"
+    },
+    {
+      name: "Dordle",
+      url: "https://zaratustra.itch.io/dordle",
+      description: "Podwójne Wordle"
+    },
+    {
+      name: "Waffle",
+      url: "https://wafflegame.net/daily",
+      description: "Wordle-krzyżówka w kształcie wafla"
+    },
+    {
+      name: "Woodle",
+      url: "https://play.woodle.today/",
+      description: "Wordle bez wskazywania konkretnych liter"
+    },
+    {
+      name: "Betweenle",
+      url: "https://betweenle.com/",
+      description: "Odgadnij wyraz alfabetycznie"
+    },
+    {
+      name: "Chronophoto",
+      url: "https://www.chronophoto.app/game.html",
+      description: "Podaj rok wykonania zdjęcia"
+    },
+    {
+      name: "TimeGuessr",
+      url: "https://timeguessr.com/",
+      description: "Zgadnij czas i miejsce wykonania zdjęcia"
+    },
+    {
+      name: "xdle",
+      url: "https://zaratustra.itch.io/xdle",
+      description: "Odgadnij liczbę x"
+    },
+    {
+      name: "Stacked",
+      url: "https://stacked.clevergoat.com/",
+      description: "Połącz wyrazy w grupy różnych wielkości"
+    },
+    {
+      name: "23 Words",
+      url: "https://wordnerd.co/23words/",
+      description: "Odshuffluj litery w 23 wyrazach"
+    },
+    {
+      name: "Disorderly",
+      url: "https://playdisorderly.com/",
+      description: "Połącz wyrazy w grupy takie same"
+    },
+    {
+      name: "Travle",
+      url: "https://travle.earth/?travle-lang=en",
+      description: "Połącz dwa kraje na mapie"
+    },
+    {
+      name: "ChessGuessr",
+      url: "https://www.chessguessr.com/",
+      description: "Podaj najlepszą sekwencję ruchów"
+    },
+    {
+      name: "Word Grid",
+      url: "https://wordgrid.clevergoat.com/",
+      description: "Uzupełnij pola wyrazami"
+    },
+    {
+      name: "Bracket City",
+      url: "https://www.theatlantic.com/games/bracket-city/",
+      description: "Odgaduj hasła od środka"
+    },
+    {
+      name: "Contexto",
+      url: "https://contexto.me/en/",
+      description: "Odgadnij tajemniczy wyraz"
+    },
+    {
+      name: "Semantle",
+      url: "https://semantle.com/",
+      description: "Odganij tajemniczy wyraz... znowu"
+    },
+    {
+      name: "Nerdle",
+      url: "https://nerdlegame.com/",
+      description: "Odganij równanie dnia"
+    },
+    {
+      name: "Fill-in-the-blank Mini",
+      url: "https://www.nytimes.com/crosswords/game/special/fill-in-the-blank",
+      description: "Krzyżówka mini z brakującymi słowami"
+    },
+    {
+      name: "Cinematrix",
+      url: "https://www.vulture.com/article/daily-movie-grid-trivia-game-cinematrix.html",
+      description: "Uzupełnij pola filmami"
+    },
+    {
+      name: "Movie Grid",
+      url: "https://moviegrid.io/",
+      description: "Uzupełnij pola filmami... znowu"
+    },
+    {
+      name: "Box Office Game",
+      url: "https://boxofficega.me/",
+      description: "Odgadnij filmy z box office"
+    },
+    {
+      name: "FoodGuessr",
+      url: "https://www.foodguessr.com/",
+      description: "Odgadnij kraj pochodzenia potrawy"
+    },
+    {
+      name: "Spellcheck",
+      url: "https://spellcheck.xyz/solo_game",
+      description: "Czy potrafisz literować?"
     }
   ];
 </script>
 
 <section>
-  <h2>🎮 Człowiek jest tym, z czym się zmaga</h2>
+  <h2>🎮 Gierki</h2>
   <ul style="list-style: none; padding-left: 0;">
     {#each games as game}
       <li style="margin-bottom: 0.75rem;">
